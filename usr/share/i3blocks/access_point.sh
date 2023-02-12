32f99395ff78bf2398e47bfd088922f61f1a1c01
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2023-02-12 21:28:47 +0100

    Add files via upload

---
+++ usr/share/i3blocks/access_point.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#ADD as ~/.config/bin/access_point.sh
+
+AP=$(/usr/sbin/route | grep default | awk '{print $2}')
+if [[ "$AP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+  echo "$(/usr/sbin/route | grep default | awk '{print $2}')"
+else 
+  echo "Disconnected"
+fi
---
+++ usr/share/i3blocks/hackthebox_status.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ADD as ~/.config/bin/hackthebox_status.sh
+ 
+#IF=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
+
+
+#[[ ! -d /sys/class/net/${IF} ]] && exit
+
+
+#if [[ "$(cat /sys/class/net/$IF/operstate)" = 'up' ]]; then
+
+
+
+#!/bin/bash
+# ADD as ~/.config/bin/hackthebox_status.sh
+ 
+IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
+ 
+if [ "$IFACE" = "tun0" ]; then
+  echo "$(/usr/sbin/ifconfig | grep tun0 -A1 | grep inet | awk '{print $2}')"
+else 
+  echo "Disconnected"
+fi
+#if [ "$IFACE" = "tun0" ]; then
+ 
