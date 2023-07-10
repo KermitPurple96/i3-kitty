4ab93c8fdbc07fe6f83d2f5d7b8fcdfdf5bfa00a
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2023-07-10 19:47:42 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,28 +8,22 @@ wget https://github.com/lsd-rs/lsd/releases/download/0.23.1/lsd-musl_0.23.1_amd6
 wget https://github.com/sharkdp/bat/releases/download/v0.23.0/bat_0.23.0_amd64.deb && sudo dpkg -i bat_0.23.0_amd64.deb
 
 mkdir ~/Descargas/firefox && mkdir ~/.zsh
-
 cd ~/.zsh && git clone https://github.com/zsh-users/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git && git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
-
 cd ~ && rm ~/.fehbg && wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/.fehbg -O ~/fehbg
-
 mkdir ~/.wallpaper && wget https://github.com/KermitPurple96/i3-kitty/blob/main/fondo.jpg -O /home/kermit/.wallpaper/fondo.jpg
-
 rm ~/.zshrc && wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/.zshrc && sudo ln -s -f ~/.zshrc /root/.zshrc
 
 mkdir ~/maquinas
-
 mkdir ~/.config/bin
-
 cd ~/.config/bin && touch {name.txt,target.txt,ttl.txt,target_sys.txt}
 
 cd /usr/share/i3blocks && sudo wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/target_sys.sh && sudo wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/target.sh && sudo wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/hackthebox_status.sh && sudo wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/ethernet_status.sh && sudo wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/access_point.sh && sudo chmod +x *
-
 mkdir ~/.config/kitty && cd ~/.config/kitty && sudo apt-get -y install kitty && sudo wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/kitty/color.ini && sudo wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/kitty/kitty.conf
-
 cd ~/.config/i3 && sudo rm -rf ~/.config/i3/* && sudo wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/i3/i3blocks.conf && sudo wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/i3/config && sudo wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/i3/clipboard_fix.sh
-
 sudo apt install xclip && sudo chown -R root:root ~/.zsh && sudo chown kermit:kermit /etc/hosts && sudo chmod -R 755 ~/.zsh && sudo usermod --shell /usr/bin/zsh kermit && sudo usermod --shell /usr/bin/zsh root
+cd /root/.config/kitty
+cp /home/kermit/.config/kitty/* .
+
 
 cd /usr/local/share/fonts && sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip && sudo unzip Hack.zip && sudo rm Hack.zip
 
