d2514e8a1f7204b2c34db3abed087105c81682b8
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2023-08-24 01:45:10 +0200

    Delete basic.sh

--- basic.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get update && \
-sudo apt-get upgrade && \
-sudo apt-get -y install kitty keepass2 moreutils xclip ftp exploitdb locate netdiscover feh rdesktop snmp enum4linux dirsearch docker.io scrub jq apache2 ncat ntpdate rlwrap metasploit-framework ipcalc xsltproc swaks flameshot ghex hexedit && \
-gem install evil-winrm && \
-apt install neo4j bloodhound && \
-sudo apt-get install powershell-empire starkiller -y && \
-apt install krb5-user
