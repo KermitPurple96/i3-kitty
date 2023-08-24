3c5a6660587892e3642e6850bc41a187f14f2645
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2023-08-24 03:03:34 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,103 +6,103 @@ apt install neo4j bloodhound && \
 sudo apt-get install powershell-empire starkiller -y && \
 apt install krb5-user
 
-git clone https://github.com/KermitPurple96/kali-clean && \
-chmod +x ./kali-clean/install.sh -y && \
-./kali-clean/install.sh -y && \
+git clone https://github.com/KermitPurple96/kali-clean
+chmod +x /home/$SUDO_USER/kali-clean/install.sh
+/home/$SUDO_USER/kali-clean/install.sh -y
 
-wget https://github.com/lsd-rs/lsd/releases/download/0.23.1/lsd-musl_0.23.1_amd64.deb -y && \
-sudo dpkg -i lsd-musl_0.23.1_amd64.deb -y && \
-wget https://github.com/sharkdp/bat/releases/download/v0.23.0/bat_0.23.0_amd64.deb -y && \
-sudo dpkg -i bat_0.23.0_amd64.deb -y && \
+wget https://github.com/lsd-rs/lsd/releases/download/0.23.1/lsd-musl_0.23.1_amd64.deb -y
+sudo dpkg -i lsd-musl_0.23.1_amd64.deb -y
+wget https://github.com/sharkdp/bat/releases/download/v0.23.0/bat_0.23.0_amd64.deb -y
+sudo dpkg -i bat_0.23.0_amd64.deb -y
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mkdir /home/$SUDO_USER/Descargas/firefox && \
-mkdir /home/$SUDO_USER/.zsh && \
-git clone https://github.com/zsh-users/zsh-autosuggestions /home/$SUDO_USER/.zsh/zsh-autosuggestions && \
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$SUDO_USER/.zsh/zsh-syntax-highlighting && \
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git /home/$SUDO_USER/.zsh/zsh-autocomplete && \
+mkdir /home/$SUDO_USER/Descargas/firefox
+mkdir /home/$SUDO_USER/.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions /home/$SUDO_USER/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$SUDO_USER/.zsh/zsh-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git /home/$SUDO_USER/.zsh/zsh-autocomplete
 
-rm /home/$SUDO_USER/.fehbg && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/.fehbg -O /home/$SUDO_USER/.fehbg \
-mkdir /home/$SUDO_USER/.wallpaper && \
-wget https://github.com/KermitPurple96/i3-kitty/blob/main/fondo.jpg -O /home/$SUDO_USER/.wallpaper/fondo.jpg && \
-rm /home/$SUDO_USER/.zshrc && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/.zshrc && \
-ln -s -f /home/$SUDO_USER/.zshrc /root/.zshrc && \
+rm /home/$SUDO_USER/.fehbg
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/.fehbg -O /home/$SUDO_USER/.fehbg
+mkdir /home/$SUDO_USER/.wallpaper
+wget https://github.com/KermitPurple96/i3-kitty/blob/main/fondo.jpg -O /home/$SUDO_USER/.wallpaper/fondo.jpg
+rm /home/$SUDO_USER/.zshrc
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/.zshrc
+ln -s -f /home/$SUDO_USER/.zshrc /root/.zshrc
 
-mkdir /home/$SUDO_USER/maquinas && \
-mkdir /home/$SUDO_USER/.config/bin && \
+mkdir /home/$SUDO_USER/maquinas
+mkdir /home/$SUDO_USER/.config/bin
 touch /home/$SUDO_USER/.config/bin/{name.txt,target.txt,ttl.txt,target_sys.txt}
 
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/target_sys.sh -O /usr/share/i3blocks/target_sys.sh && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/target.sh -O /usr/share/i3blocks/target.sh && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/hackthebox_status.sh -O /usr/share/i3blocks/hackthebox_status.sh && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/ethernet_status.sh -O /usr/share/i3blocks/ethernet_status.sh && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/access_point.sh -O /usr/share/i3blocks/access_point.sh && \
-sudo chmod +x /usr/share/i3blocks/* && \
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/target_sys.sh -O /usr/share/i3blocks/target_sys.sh
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/target.sh -O /usr/share/i3blocks/target.sh
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/hackthebox_status.sh -O /usr/share/i3blocks/hackthebox_status.sh
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/ethernet_status.sh -O /usr/share/i3blocks/ethernet_status.sh
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/usr/share/i3blocks/access_point.sh -O /usr/share/i3blocks/access_point.sh
+sudo chmod +x /usr/share/i3blocks/*
 
-mkdir /home/$SUDO_USER/.config/kitty && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/kitty/color.ini -O /home/$SUDO_USER/.config/kitty/color.ini && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/kitty/kitty.conf -O /home/$SUDO_USER/.config/kitty/kitty.conf && \
+mkdir /home/$SUDO_USER/.config/kitty
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/kitty/color.ini -O /home/$SUDO_USER/.config/kitty/color.ini
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/kitty/kitty.conf -O /home/$SUDO_USER/.config/kitty/kitty.conf
 
-rm -rf /home/$SUDO_USER/.config/i3/* && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/i3/i3blocks.conf -O /home/$SUDO_USER/.config/i3/i3blocks.conf && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/i3/config && -O /home/$SUDO_USER/.config/i3/config && \
-wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/i3/clipboard_fix.sh -O /home/$SUDO_USER/.config/i3/clipboard_fix.sh && \
+rm -rf /home/$SUDO_USER/.config/i3/*
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/i3/i3blocks.conf -O /home/$SUDO_USER/.config/i3/i3blocks.conf
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/i3/config && -O /home/$SUDO_USER/.config/i3/config
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/i3/clipboard_fix.sh -O /home/$SUDO_USER/.config/i3/clipboard_fix.sh
 
-chown -R root:root /home/$SUDO_USER/.zsh && \ 
-chown $SUDO_USER:$SUDO_USER /etc/hosts && \
-sudo chmod -R 755 /home/$SUDO_USER/.zsh && \
-sudo usermod --shell /usr/bin/zsh $SUDO_USER && sudo usermod --shell /usr/bin/zsh root && \
-cp /home/$SUDO_USER/.config/kitty/* /root/.config/kitty && \
+chown -R root:root /home/$SUDO_USER/.zsh
+chown $SUDO_USER:$SUDO_USER /etc/hosts
+sudo chmod -R 755 /home/$SUDO_USER/.zsh
+sudo usermod --shell /usr/bin/zsh $SUDO_USER && sudo usermod --shell /usr/bin/zsh root
+cp /home/$SUDO_USER/.config/kitty/* /root/.config/kitty
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip -O /usr/local/share/fonts/Hack.zip && \
-unzip /usr/local/share/fonts/Hack.zip && \
-rm /usr/local/share/fonts/Hack.zip && \
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip -O /usr/local/share/fonts/Hack.zip
+unzip /usr/local/share/fonts/Hack.zip
+rm /usr/local/share/fonts/Hack.zip
 
 # Red
-sudo wget https://github.com/Rvn0xsy/red-tldr/releases/download/v0.4.3/red-tldr_0.4.3_linux_amd64.tar.gz -O /usr/bin/red-tldr_0.4.3_linux_amd64.tar.gz && \
-sudo tar -zxvf /usr/bin/red-tldr*.gz && \
-/usr/bin/red-tldr update && \
-/usr/bin/red-tldr upgrade && \
-sudo mv /usr/bin/red-tldr /usr/bin/red && \
-sudo rm /usr/bin/red-tldr_0.4.3_linux_amd64.tar.gz && \
+sudo wget https://github.com/Rvn0xsy/red-tldr/releases/download/v0.4.3/red-tldr_0.4.3_linux_amd64.tar.gz -O /usr/bin/red-tldr_0.4.3_linux_amd64.tar.gz
+sudo tar -zxvf /usr/bin/red-tldr*.gz
+/usr/bin/red-tldr update
+/usr/bin/red-tldr upgrade
+sudo mv /usr/bin/red-tldr /usr/bin/red
+sudo rm /usr/bin/red-tldr_0.4.3_linux_amd64.tar.gz
 
 # Arsenal
-git clone https://github.com/Orange-Cyberdefense/arsenal.git /usr/bin/arsenal && \
-python3 -m pip install -r /usr/bin/arsenal/requirements.txt && \
-mv /usr/bin/arsenal/run /usr/bin/arsenal/ars && \ 
-wget https://github.com/KermitPurple96/i3-kitty/blob/main/my_cheats.md -O /usr/bin/arsenal/my_cheats/my_cheats.md && \
+git clone https://github.com/Orange-Cyberdefense/arsenal.git /usr/bin/arsenal
+python3 -m pip install -r /usr/bin/arsenal/requirements.txt
+mv /usr/bin/arsenal/run /usr/bin/arsenal/ars
+wget https://github.com/KermitPurple96/i3-kitty/blob/main/my_cheats.md -O /usr/bin/arsenal/my_cheats/my_cheats.md
 
 #neovim
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O /usr/bin/nvim && \
-chmod +x /usr/bin/nvim && \
+wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O /usr/bin/nvim
+chmod +x /usr/bin/nvim
 
 # jump
-sudo wget https://github.com/gsamokovarov/jump/releases/download/v0.51.0/jump_linux_amd64_binary -O /usr/bin/jump && \
-sudo chmod +x /usr/bin/jump && \
+sudo wget https://github.com/gsamokovarov/jump/releases/download/v0.51.0/jump_linux_amd64_binary -O /usr/bin/jump
+sudo chmod +x /usr/bin/jump
 
 #crackmapexec
-python3 -m pip install pipx && \
-pipx ensurepath && \
-pipx install crackmapexec && \
+python3 -m pip install pipx
+pipx ensurepath
+pipx install crackmapexec
 
 #kerbrute
-pip3 install kerbrute -y && \
+pip3 install kerbrute -y
 
 #wpscan fix
-apt remove wpscan -y && \
-apt-get autoremove -y && \
-apt-get autoclean && \
-hash -r && \
-apt install ruby-dev -y && \
-gem install wpscan && \
+apt remove wpscan -y
+apt-get autoremove -y
+apt-get autoclean
+hash -r
+apt install ruby-dev -y
+gem install wpscan
 
 # GTFO
-sudo git clone https://github.com/mzfr/gtfo /usr/bin/gtfo && \
-chmod +x /usr/bin/gtfo && \
+sudo git clone https://github.com/mzfr/gtfo /usr/bin/gtfo
+chmod +x /usr/bin/gtfo
 
 # fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git /home/$SUDO_USER/.fzf && \
-/home/$SUDO_USER/.fzf/install -y && \
-sudo mv /home/$SUDO_USER/.fzf/bin/fzf /usr/local/bin && \
+git clone --depth 1 https://github.com/junegunn/fzf.git /home/$SUDO_USER/.fzf
+/home/$SUDO_USER/.fzf/install -y
+sudo mv /home/$SUDO_USER/.fzf/bin/fzf /usr/local/bin
