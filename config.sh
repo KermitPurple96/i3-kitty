a26e454667f13ca7786a7f20c4bb596431c5cfee
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2023-08-24 03:28:47 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,9 @@
-sudo apt-get update && \
-sudo apt-get upgrade && \
-sudo apt-get -y install kitty zsh keepass2 moreutils xclip ftp exploitdb locate netdiscover feh rdesktop snmp enum4linux dirsearch docker.io scrub jq apache2 ncat ntpdate rlwrap metasploit-framework ipcalc xsltproc swaks flameshot ghex hexedit && \
-gem install evil-winrm && \
-apt install neo4j bloodhound && \
-sudo apt-get install powershell-empire starkiller -y && \
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install kitty zsh keepass2 moreutils xclip ftp exploitdb locate netdiscover feh rdesktop snmp enum4linux dirsearch docker.io scrub jq apache2 ncat ntpdate rlwrap metasploit-framework ipcalc xsltproc swaks flameshot ghex hexedit
+gem install evil-winrm
+apt install neo4j bloodhound
+sudo apt-get install powershell-empire starkiller -y
 apt install krb5-user
 
 git clone https://github.com/xct/kali-clean
