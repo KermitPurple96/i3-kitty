e8a5465f6c5c831fc88a0ecdfb33d83895081c0d
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2023-08-31 16:48:46 +0200

    Update hackthebox_status.sh

--- usr/share/i3blocks/hackthebox_status.sh
+++ usr/share/i3blocks/hackthebox_status.sh
@@ -15,11 +15,17 @@
 # ADD as ~/.config/bin/hackthebox_status.sh
  
 IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
+IFACE2=$(/usr/sbin/ifconfig | grep tap0 | awk '{print $1}' | tr -d ':')
+
  
 if [ "$IFACE" = "tun0" ]; then
   echo "$(/usr/sbin/ifconfig | grep tun0 -A1 | grep inet | awk '{print $2}')"
-else 
+else
+  echo "Disconnected"
+fi
+if [ "$IFACE2" = "tap0" ]; then
+  echo "$(/usr/sbin/ifconfig | grep tap0 -A1 | grep inet | awk '{print $2}')"
+else
   echo "Disconnected"
 fi
 #if [ "$IFACE" = "tun0" ]; then
- 
