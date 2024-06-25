090c3800cf5e1f30c18a2fe4ab329bde96a6fe42
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2024-06-25 18:20:56 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -264,6 +264,9 @@ rm /home/$SUDO_USER/LICENSE.md
 # chsh -s /usr/bin/fish
 # edit .config/kitty/kitty.conf 
 
+wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/fish/config.fish -O /home/$SUDO_USER/.config/fish/config.fish
+sudo ln -s -f /home/$SUDO_USER/.config/fish/config.fish /root/.config/fish/config.fish
+
 wget https://raw.githubusercontent.com/KermitPurple96/i3-kitty/main/ascii -O /home/$SUDO_USER/ascii
 
 
