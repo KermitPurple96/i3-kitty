bea58aa72f8b5c77d0a7ecf3e686d26177267a8c
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2024-07-17 23:49:43 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ cd kali-clean
 #packages
 sudo apt install proxychains dnsmasq dnscat2 curl ncat dnsrecon enum4linux feroxbuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf
 sudo apt install autorecon
-sudo apt-get -y install seclists putty-tools powercat lolcat neofetch build-essential gcc feroxbuster pipx zsh keepass2 moreutils xclip ftp exploitdb locate netdiscover feh rdesktop snmp enum4linux dirsearch docker.io scrub jq apache2 ncat ntpdate rlwrap metasploit-framework ipcalc xsltproc swaks flameshot ghex hexedit
+sudo apt-get -y install seclists putty-tools powercat lolcat neofetch build-essential gcc feroxbuster pipx zsh keepass2 moreutils xclip ftp exploitdb locate netdiscover feh rdesktop snmp enum4linux dirsearch docker-compose docker.io scrub jq apache2 ncat ntpdate rlwrap metasploit-framework ipcalc xsltproc swaks flameshot ghex hexedit
 sudo apt-get update && sudo apt-get -y install golang-go 
 python3 -m pip install --user pwntools
 
