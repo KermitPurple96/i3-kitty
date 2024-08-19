48e34e77159760aaffe27becf72594851c042618
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2024-08-19 20:54:52 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -33,6 +33,7 @@ mkdir /home/$SUDO_USER/dev/python
 git clone https://github.com/KermitPurple96/Shellpy -O /home/$SUDO_USER/dev/python/shellpy
 git clone https://github.com/KermitPurple96/OSCP-PythonSupportTools -O /home/$SUDO_USER/dev/python/support
 wget https://raw.githubusercontent.com/KermitPurple96/scripts/main/Python/md4.py -O /home/$SUDO_USER/dev/python/md4
+wget https://raw.githubusercontent.com/mubix/IOXIDResolver/main/IOXIDResolver.py -O /home/$SUDO_USER/dev/python/IOXIDResolver.py
 chmod -R +x /home/$SUDO_USER/dev/python/*
 
 mkdir /home/$SUDO_USER/dev/go
