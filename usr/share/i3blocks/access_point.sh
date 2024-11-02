d7b0b9ae778322b01503fe7e38ec2fbf2ff8864b
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2024-11-02 13:44:52 +0100

    Update access_point.sh

--- usr/share/i3blocks/access_point.sh
+++ usr/share/i3blocks/access_point.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 INTERFACE=$(cat /usr/share/i3blocks/iface)
-# Filtra las líneas correspondientes a la interfaz, excluye 0.0.0.0 si hay otra opción
-AP=$( /usr/sbin/route | grep "$INTERFACE" | awk '{print $2}' | grep -v "^0\.0\.0\.0" | head -n 1 )
+IP=$(/usr/sbin/ifconfig | grep "$INTERFACE" -A1 | grep inet | awk '{print $2}')
 
-# Si AP está vacío, selecciona 0.0.0.0 como última opción
-if [[ -z "$AP" ]]; then
-  AP=$( /usr/sbin/route | grep "$INTERFACE" | awk '{print $2}' | grep "^0\.0\.0\.0" | head -n 1 )
-fi
-
-# Verifica si AP tiene una IP válida
-if [[ "$AP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-  echo "$AP"
+if [[ "$IP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+  echo "$IP"
 else
   echo "Disconnected"
 fi
