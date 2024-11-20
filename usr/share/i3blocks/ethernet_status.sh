bc7b8d8b8f01f88c708a59bc77b60a9023edc68e
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2024-11-20 19:03:06 +0100

    Update ethernet_status.sh

--- usr/share/i3blocks/ethernet_status.sh
+++ usr/share/i3blocks/ethernet_status.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #ADD as ~/.config/bin/ethernet_status.sh
 
-IP=$(/usr/sbin/ifconfig | grep ens33 -A1 | grep inet | awk '{print $2}')
+INTERFACE=$(cat /usr/share/i3blocks/iface)
+IP=$(/usr/sbin/ifconfig | grep "$INTERFACE" -A1 | grep inet | awk '{print $2}')
 if [[ "$IP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-  echo "$(/usr/sbin/ifconfig | grep ens33 -A1 | grep inet | awk '{print $2}')"
+  echo "$(/usr/sbin/ifconfig | grep "$INTERFACE" -A1 | grep inet | awk '{print $2}')"
 else 
   echo "Disconnected"
 fi
