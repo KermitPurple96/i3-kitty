81d87e221a46790a7500b80d7e43ba618f7b16a7
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2024-11-20 18:44:02 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -225,10 +225,16 @@ curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | 
 sudo apt install pipx git
 pipx ensurepath
 pipx install git+https://github.com/Pennyw0rth/NetExec
+
+#poetry netexec
 pipx install poetry
 poetry self add "poetry-dynamic-versioning[plugin]"
 poetry dynamic-versioning enable
 
+git clone https://github.com/Pennyw0rth/NetExec
+cd NetExec
+poetry install
+poetry run NetExec
 
 #impacket pipx
 #python3 -m pip install pipx
