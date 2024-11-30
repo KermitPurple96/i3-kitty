c52892207f0554c7571724f1105c0d499f22f93c
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2024-11-30 10:25:44 +0100

    Update access_point.sh

--- usr/share/i3blocks/access_point.sh
+++ usr/share/i3blocks/access_point.sh
@@ -6,5 +6,5 @@ GATEWAY=$(ip route show dev "$INTERFACE" | grep default | awk '{print $3}')
 if [[ -n "$GATEWAY" ]]; then
   echo "Gateway: $GATEWAY"
 else
-  echo "No gateway found or disconnected."
+  echo "No gateway"
 fi
