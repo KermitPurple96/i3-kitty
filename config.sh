a546ff59a2cdeda1df1f1d9553f5080091b37194
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2025-05-02 16:28:54 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -18,10 +18,14 @@ rm -rf kali-clean
 sudo apt install libsasl2-dev python-dev-is-python3 libldap2-dev libssl-dev snmp-mibs-downloader meson proxychains dnsmasq ripgrep smtp-user-enum dnscat2 curl ncat dnsrecon enum4linux feroxbuster impacket-scripts nbtscan nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf autorecon
 sudo apt-get -y install gh kitty zaproxy seclists putty-tools powercat lolcat httptunnel neofetch build-essential grc gcc cryptsetup steghide feroxbuster hping3 pipx zsh remmina keepass2 moreutils xclip ftp exploitdb locate netdiscover feh rdesktop snmp dirsearch docker-compose docker.io scrub jq apache2 ncat ntpdate rlwrap metasploit-framework ipcalc xsltproc swaks flameshot ghex hexedit
 sudo apt-get update && sudo apt-get -y install golang-go 
-sudo apt install libcriterion-dev clang npm cmake
+sudo apt install libcriterion-dev clang npm nodejs cmake
+
 npm -g install js-beautify
 npm install -g pyright
+npm install -g vscode-langservers-extracted
+
 go install github.com/jesseduffield/lazygit@latest
+
 apt update
 apt upgrade
 python3 -m pip install --user pwntools
